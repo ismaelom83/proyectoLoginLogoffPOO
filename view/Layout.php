c27efab9bd20e238ecf16697113cddf3823fd212
Miguel <miguela.aragar@educa.jcyl.es>
2020-01-20 08:56:02 +0100

    empezando a programar

--- view/Layout.php
+++ view/Layout.php
@@ -43,7 +43,7 @@
         </main>    
          <footer class="page-footer font-small blue load-hidden">
             <div class="footer-copyright text-center py-3"> <a href="../../../index.php">© 2019 Copyright: Ismael Heras Salvador</a> 
-                <a href="http://daw-usgit.sauces.local/heras/LoginLogoffMulticapaPOO/tree/master" target="_blank" ><img  src="WEBBROOT/img/gitLab.png" alt=GitLab""></a>
+                <a href="http://daw-usgit.sauces.local/heras/proyectoLoginLogoffPOO/tree/master" target="_blank" ><img  src="WEBBROOT/img/gitLab.png" alt=GitLab""></a>
                 <a href="https://github.com/ismaelom83/proyectoLoginLogoffPOO" target="_blank" ><img  src="WEBBROOT/img/gitHub.png" alt="GitHub"></a>
                 <a href="DOC/loginphpdocumentor/index.html"><img  src="WEBBROOT/img/documentor.png" alt="documeentor"></a>
                 <a href="DOC/GoF Design Patterns - Composite.vpd.pdf">Diagrama de clases</a>
