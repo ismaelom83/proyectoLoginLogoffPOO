3e54a557855ddb307e22a632590c6276e20ad2b7
Miguel <miguela.aragar@educa.jcyl.es>
2020-01-21 12:24:48 +0100

    empezando a programar

--- model/UsuarioPDO.php
+++ model/UsuarioPDO.php
@@ -34,9 +34,10 @@ class UsuarioPDO {
         $resConsulta = DBPDO::ejecutaConsulta($consulta, [$codUsuario, $password]);
         //Si ahya algun resultado alamacenamos en el array todos los resultados.
         if ($resConsulta->rowCount() == 1) {
-             //update a la base de datos para amuntar el numero de visitas
+             //update a la base de datos para amuntar el numero de visitas antes de crear ekl objeto
             $consulta = "UPDATE T01_Usuarios SET T01_NumAccesos=T01_NumAccesos+1, T01_FechaHoraUltimaConexion=now() WHERE T01_CodUsuario=?";
             DBPDO::ejecutaConsulta($consulta, [$codUsuario]);
+            //creamos el objeto usuario
        $usuario =$resConsulta->fetchObject();
        $obUsuario = new Usuario($usuario->T01_CodUsuario,$usuario->T01_DescUsuario,$usuario->T01_Password,$usuario->T01_Perfil,$usuario->T01_FechaHoraUltimaConexion,$usuario->T01_NumAccesos);
        return $obUsuario;
